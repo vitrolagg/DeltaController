400392d3775c93154efdf6f0cd81dbeacf5eb3e7
Vitor Gabriel <vitrolagg@gmail.com>
2025-07-14 10:31:36 -0300

    Adição do arquivo de teste

---
+++ test/test.py
@@ -0,0 +1,9 @@
+from DeltaController import DeltaRobot
+
+# Dimensões do robo
+e = 43.3
+f = 173.205
+rf = 310
+re = 120
+
+robot1 = DeltaRobot(base_radius=e, gripper_radius=f, active_arm=rf, passive_arm=re)
